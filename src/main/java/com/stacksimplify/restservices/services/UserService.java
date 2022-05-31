355d35c4d37ab8211cbc297cbd45b4f536a08f0d
giuliasferber <giuliasferrazzo@gmail.com>
2022-05-31 15:53:46 +0200

    ResponseStatusException First Commit

--- src/main/java/com/stacksimplify/restservices/services/UserService.java
+++ src/main/java/com/stacksimplify/restservices/services/UserService.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.stacksimplify.restservices.entities.User;
+import com.stacksimplify.restservices.exceptions.UserExistsException;
+import com.stacksimplify.restservices.exceptions.UserNotFoundException;
 import com.stacksimplify.restservices.repositories.UserRepository;
 
 @Service
@@ -24,22 +28,36 @@ public class UserService {
 	}
 	
 	//createUser Method
-	public User createUser(User user) {
-		
+	public User createUser(User user) throws UserExistsException {
+		String username = user.getUsername();
+		User existingUser = userRepository.findByUsername(username);
+		if(existingUser != null) {
+			throw new UserExistsException("User already exists in repository");
+		}
 		return userRepository.save(user);
 	
 	}
 	
 	//getUserById Method
-	public Optional<User> getUserById(Long id) {
+	public Optional<User> getUserById(Long id) throws UserNotFoundException{
 		
 		Optional<User> user = userRepository.findById(id);
+		
+		if(!user.isPresent()) {
+			throw new UserNotFoundException("User not found in User Repository");
+		}
+		
 		return user;
 		
 	}
 	
 	//updateUserById Method
-	public User updateUserById(Long id, User user) {
+	public User updateUserById(Long id, User user) throws UserNotFoundException{
+		
+		Optional<User> optionalUser = userRepository.findById(id);
+		if(!optionalUser.isPresent()) {
+			throw new UserNotFoundException("User not found in User Repository, provide the correct user id");
+		}
 		
 		user.setId(id);
 		return userRepository.save(user);
@@ -49,9 +67,12 @@ public class UserService {
 	//deleteUserById Method
 	public void deleteUserById(Long id) {
 		
-		if(userRepository.findById(id).isPresent()) {
-			userRepository.deleteById(id);
+		Optional<User> optionalUser = userRepository.findById(id);
+		if(!optionalUser.isPresent()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User not found in User Repository, provide the correct user id");
 		}
+		
+		userRepository.deleteById(id);
 		
 	}
 	
